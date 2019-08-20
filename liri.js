1d53cd273adcf9e75b31d6657c99f77f74829ddd
Jon De Jesus <jondejesus17@gmail.com>
2019-08-19 22:59:14 -0700

    required files to be referenced

--- liri.js
+++ liri.js
@@ -1,2 +1,9 @@
-require("dotenv").config();
-var keys = require("./keys.js");
+var keys = require("./keys");
+var axios = require("axios");
+var moment = require("moment");
+var Spotify = require('node-spotify-api');
+var fs = require("fs");
+var spotify = new Spotify({
+  id: keys.spotify.id,
+  secret: keys.spotify.secret,
+});
