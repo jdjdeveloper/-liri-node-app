08dbc1ccf8b582d0bd40101fa138aba6eba3e8d3
Jon De Jesus <jondejesus17@gmail.com>
2019-08-19 23:42:56 -0700

    movie function?

--- liri.js
+++ liri.js
@@ -20,8 +20,36 @@ switch (inputTopic){
         break;
     
     case "movie-this":
+        movieThis()
         break;
 
     case "do-what-it-says":
         break;
+}
+
+//Get movies
+function movieThis(movie) {
+    axios.get("http://www.omdbapi.com/?t=" + movie + "&y=&plot=short&tomatoes=true&apikey=trilogy").then(
+        function(response) {
+            //console.log(response.data);
+            if (response.data.Title != undefined) {
+                console.log("Title: " + response.data.Title);
+                console.log("Year: " + response.data.Year);
+                console.log("imdbRating:: " + response.data.imdbRating);
+                console.log("Title: " + response.data.Title);
+                console.log("Country:: " + response.data.Country);
+                console.log("Language:: " + response.data.Language);
+                console.log("Plot: " + response.data.Plot);
+                console.log("Actors: " + response.data.Actors);
+                console.log("RottenTomatoes: " + response.data.tomatoRating);
+            } 
+            else {
+                movieThis("Mr. Nobody");
+            }
+        }
+        // if response is empty call the api again with the "default" movie 
+    ).catch(function (error) {  
+        console.log(error);
+        console.log("No Results found. ");
+  });
 }
