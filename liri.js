f6dfd91bf65fbde96d8816e9617642bbff722446
Jon De Jesus <jondejesus17@gmail.com>
2019-08-19 23:09:10 -0700

    Getting inputs to do the things to say

--- liri.js
+++ liri.js
@@ -7,3 +7,21 @@ var spotify = new Spotify({
   id: keys.spotify.id,
   secret: keys.spotify.secret,
 });
+
+var userInput = process.argv;
+var inputTopic = process.argv[2];
+
+
+switch (inputTopic){
+    case "concert-this":
+        break;
+    
+    case "spotify-this-song":
+        break;
+    
+    case "movie-this":
+        break;
+
+    case "do-what-it-says":
+        break;
+}
